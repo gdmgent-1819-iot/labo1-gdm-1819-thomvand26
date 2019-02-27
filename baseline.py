dd85706a4c5a3b0396eb101cddaf95dc5c468ba8
gdm-1819-thomvand26 <thomvand26@student.arteveldehs.be>
2019-02-27 20:43:12 +0100

    baseline.py update

--- baseline.py
+++ baseline.py
@@ -7,7 +7,7 @@ text = 'Hello! We are New Media Development :)'
 
 while True:
     try:
-        sense.show_message(text, back_colour = [randint(50, 100), randint(50, 100), randint(50, 100)], text_colour = [randint(200, 255), randint(200, 255), randint(200, 255)])
+        sense.show_message(text, back_colour = [randint(20, 70), randint(20, 70), randint(20, 70)], text_colour = [randint(200, 255), randint(200, 255), randint(200, 255)])
     except KeyboardInterrupt:
         sense.clear()
         sys.exit()
