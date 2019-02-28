9d2301b72a7bf2aabea988df77e4f65dc54e93bd
gdm-1819-thomvand26 <thomvand26@student.arteveldehs.be>
2019-02-28 19:22:49 +0100

    rect_animated.py test

---
+++ rect_animated.py
@@ -0,0 +1,34 @@
+from random import randint
+from sense_hat import SenseHat
+import sys
+import time
+
+sense = SenseHat()
+rectColour = (150,150,150)
+
+while True:
+    try:
+        # for y in range(8):
+        #     for x in range(8):
+        #         sense.set_pixel(x, y, randint(0, 255), randint(0, 255), randint(0, 255))
+        #         time.sleep(0.2)
+        #         sense.set_pixel(x, y, 0, 0, 0)
+
+        #         if x + y == 18:
+        #             x = 0
+        #             y = 0
+        createRect(0,0,8,8)
+    except KeyboardInterrupt:
+        sense.clear()
+        sys.exit()
+
+def createRect(originX, originY, width, height):
+
+    for y in range(8):
+        if originY <= y <= (originY + y):
+            for x in range(8):
+                if originX <= x <= (originX + width):
+                    if (y == originY) or (y == originY + height):
+                        sense.set_pixel(x, y, rectColour)
+                    elif (x == originX) or (x == originY + width):
+                        sense.set_pixel(x, y, rectColour)
