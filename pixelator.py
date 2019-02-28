33703d5ebca2410f3c0b178df4cd3a7158ae9638
gdm-1819-thomvand26 <thomvand26@student.arteveldehs.be>
2019-02-28 18:41:22 +0100

    pixelator.py update

--- pixelator.py
+++ pixelator.py
@@ -7,13 +7,13 @@ sense = SenseHat()
 
 while True:
     try:
-        for y in range(7):
-            for x in range(7):
-                sense.set_pixel(y, x, randint(0, 255), randint(0, 255), randint(0, 255))
+        for y in range(1,8):
+            for x in range(1,8):
+                sense.set_pixel(x, y, randint(0, 255), randint(0, 255), randint(0, 255))
                 time.sleep(0.2)
-                sense.set_pixel(y, x, 0, 0, 0)
+                sense.set_pixel(x, y, 0, 0, 0)
 
-                if x + y == 14:
+                if x + y == 16:
                     x = 0
                     y = 0
     except KeyboardInterrupt:
