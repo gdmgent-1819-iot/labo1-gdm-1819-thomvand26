c361feea689642610214ff30978aaab38af61e97
gdm-1819-thomvand26 <thomvand26@student.arteveldehs.be>
2019-03-01 15:49:23 +0100

    avatar_animated.py update

--- avatar_animated.py
+++ avatar_animated.py
@@ -1,20 +1,27 @@
 from random import randint
 from sense_hat import SenseHat
-from time import sleep
+import sys
+from time
 
 sense = SenseHat()
 
 colour1 = (randint(0, 255), randint(0, 255), randint(0, 255))
 colour2 = (randint(0, 255), randint(0, 255), randint(0, 255))
 
-for y in range(8):
-    for x in range(4):
-        randNum = randint(0,1)
-        if randNum == 0:
-            sense.set_pixel(x, y, colour1)
-            sense.set_pixel((7-x), y, colour1)
-        else:
-            sense.set_pixel(x, y, colour2)
-            sense.set_pixel((7-x), y, colour2)
-sleep(1)
-sense.clear()
+while True:
+    try:
+        for y in range(8):
+            for x in range(4):
+                randNum = randint(0,1)
+                if randNum == 0:
+                    sense.set_pixel(x, y, colour1)
+                    sense.set_pixel((7-x), y, colour1)
+                else:
+                    sense.set_pixel(x, y, colour2)
+                    sense.set_pixel((7-x), y, colour2)
+                time.sleep(0.2)
+        time.sleep(1)
+        sense.clear()
+    except KeyboardInterrupt:
+        sense.clear()
+        sys.exit()
