bdd30a02d108055f399f8635906b38d12e98008c
gdm-1819-thomvand26 <43472781+gdm-1819-thomvand26@users.noreply.github.com>
2019-03-01 16:12:31 +0100

    baseline.py update

--- baseline.py
+++ baseline.py
@@ -1,6 +1,5 @@
 from random import randint
 from sense_hat import SenseHat
-import sys
 
 sense = SenseHat()
 text = 'Hello! We are New Media Development :)'
@@ -10,4 +9,4 @@ while True:
         sense.show_message(text, back_colour = [randint(20, 70), randint(20, 70), randint(20, 70)], text_colour = [randint(200, 255), randint(200, 255), randint(200, 255)])
     except KeyboardInterrupt:
         sense.clear()
-        sys.exit()
+        quit()
