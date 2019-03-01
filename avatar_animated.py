6a6cc9777d1eb62b22a2611b1bdb95ff79d882af
gdm-1819-thomvand26 <thomvand26@student.arteveldehs.be>
2019-03-01 15:45:14 +0100

    avatar_animated.py test

---
+++ avatar_animated.py
@@ -0,0 +1,20 @@
+from random import randint
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+
+colour1 = (randint(0, 255), randint(0, 255), randint(0, 255))
+colour2 = (randint(0, 255), randint(0, 255), randint(0, 255))
+
+for y in range(8):
+    for x in range(4):
+        randNum = randint(0,1)
+        if randNum == 0:
+            sense.set_pixel(x, y, colour1)
+            sense.set_pixel((7-x), y, colour1)
+        else:
+            sense.set_pixel(x, y, colour2)
+            sense.set_pixel((7-x), y, colour2)
+sleep(1)
+sense.clear()
