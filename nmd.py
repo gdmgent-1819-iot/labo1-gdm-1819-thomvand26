39f757920897c9564512cd6e467f3e3747f4e97e
gdm-1819-thomvand26 <43472781+gdm-1819-thomvand26@users.noreply.github.com>
2019-03-01 16:03:50 +0100

    nmd.py update

--- nmd.py
+++ nmd.py
@@ -1,7 +1,6 @@
 from random import randint
 from sense_hat import SenseHat
-import sys
-import time
+from time import sleep
 
 sense = SenseHat()
 nmd = 'NMD'
@@ -10,8 +9,8 @@ while True:
     try:
         for var in nmd:
             sense.show_letter(var, text_colour = [randint(0, 255), randint(0, 255), randint(0, 255)])
-            time.sleep(1)
-        time.sleep(2)
+            sleep(1)
+        sleep(2)
     except KeyboardInterrupt:
         sense.clear()
-        sys.exit()
+        quit()
